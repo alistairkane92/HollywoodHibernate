2c83c649939381ec7ddd1a62d13089abc599c019
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-03-16 13:11:14 +0000

    creates the first test

---
+++ src/test/java/TestDBHelper.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestDBHelper {
+    Studio warnerBros = new Studio("WarnerBros", 500000000, "California");
+
+    Actor keanu = new Actor("Keanu", "Reeves", 500000);
+    Actor lawrence = new Actor("Lawrence", "Fishbourne", 200000);
+    Actor carrie = new Actor("Carrie-Ann", "Moss", 200000);
+
+    Director wachowski1 = new Director("Lilly", 50000);
+    Director wachowski2 = new Director("Lana", 50000);
+
+    Film film = new Film("The Matrix", 63000000, filmActors, directorList);
+
+    @Test
+    public void testCanSave() {
+        DBHelper.saveUpdate(keanu);
+        assertEquals(1, DBHelper.getAll(Actor));
+    }
+}
