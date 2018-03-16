9d0ec47a854ca79ed7c6cfdeab992acb0f6ec85a
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-03-16 13:16:48 +0000

    creates Studio class, packages and mapping

---
+++ src/main/java/models/Studio.java
@@ -0,0 +1,46 @@
+package models;
+
+public class Studio {
+    private int id;
+    private String name;
+    private int finances;
+    private String location;
+
+    public Studio(String name, int finances, String location) {
+        this.name = name;
+        this.finances = finances;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFinances() {
+        return finances;
+    }
+
+    public void setFinances(int finances) {
+        this.finances = finances;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
